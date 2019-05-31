71a6b1bb52a9a540a14ee9d0b16505b2c75c3fb5
George <seatonk257@gmail.com>
2019-05-31 02:07:04 -0400

    parser, but with more notes

--- wip/sorter.py
+++ wip/sorter.py
@@ -31,6 +31,7 @@ bandmin = -10
 bandmax = 0 + bando
 for i in range(10):
     band = [(x,y,z) for (x,y,z) in grid if ((bandmin<x)and(x<bandmax)) ]   # gives syntax error if for loop is excluded
+    #band.sort(key=xvalue)  # Alternatively this, but it's slower
     print(band)
     bandmin += bando
     bandmax += bando
