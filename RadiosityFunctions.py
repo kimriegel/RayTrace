4e10deb5129636b627727b5ac429239391f555d8
PhyoKyawVassar <51376552+PhyoKyawVassar@users.noreply.github.com>
2019-06-11 14:43:06 -0400

    Update

--- RadiosityFunctions.py
+++ RadiosityFunctions.py
@@ -18,69 +18,69 @@ def PATCHESSHORT(min,max,N,q,dd):
             dd[m] = k[m]*q**(m-1)
         elif (m <= N) and (m >= (N/2)):
             dd[m] = k[m]*q**(N-m)
-    return k 
+    return k
     #No errors returned. Does it work?
     #Find good values to plug
 
 
 #Unfinished
-def CREATEPATCHARRAY(ddm,ddL,Nm,ddL,x1,x2,y1,y2,z1,z2,patcharray,slope,b,slope1,b1,count,normal)
+def CREATEPATCHARRAY(ddm,ddl,Nm,Nl,x1,x2,y1,y2,z1,z2,patcharray,slope,b,slope1,b1,count,normal):
 #This function creates an array of patches for each plane
 #Raws
     double precision x1,y1,x2,y2,z1,z2,normal(3)
+    #what is normal? a function or array? If array, why specificially index 3
     real temp1(3),temp2(3),temp3(3),vec1(3),vec2(3)
+    #From Fortran, this is an array. Why is it referring to index 3.
     real vec3(3),slope,b,ddz,ddy,ddx,zcenter,xcenter,ycenter
     real slope1,b1
     double precision patcharray(Nm*ddL,6),x,y,z,ddm(Nm),ddL(ddL),d
     d = -x1*normal(1)-y1*normal(2)-z1*normal(3)
     #normal looks like a string with 3 inputs
+
 #find how to do that
-    if (z1 == z2):
+    if z1 == z2:
         count = 1
-        print('ZPLANE',ddL, Nm, x1, y1, z1)
-        for L in range (1,ddL):
+        print('ZPLANE',ddl, Nm, x1, y1, z1)
+        for L in range (1,ddl):
             for m in range(1,Nm):
-                x=x1-0.5*ddm(m)+SUM(ddm(1:m))
-                y=y1-0.5*ddl(L)+SUM(ddl(1:L))
+                x=x1-0.5*ddm[m]+sum(ddm[1:m])
+                y=y1-0.5*ddl[L]+sum(ddl[1:L])
                 #Translate SUM
-                z = (-d-normal(1)*x-normal(2)*y)/normal(3)
+                z = (-d-normal[0]*x-normal[1]*y)/normal[2]
                 if m == 1:
-                    zcenter=z
+                    zcenter = z
                 if L == 1:
-                    ddz=z-z1
+                    ddz = z-z1
                     #If loop may kill function early
                     #if problems come try While loop?
-                else
+                else:
                     ddz = 0.5*(z-zcenter)
                 if (y > slope*x+b) or (y < slope1*x+b1):
 #                    GOTO 3
 #Meep
-#                patcharray(count,1)=x 
+#                patcharray(count,1)=x
 #                patcharray(count,2)=y
 #                patcharray(count,3)=z
 #                patcharray(count,4)=ddm(m)
 #                patcharray(count,5)=ddL(L)
 #                patcharray(count,6)=ddz
-                patcharray = [x,y,z,ddm(m),ddL(L),ddz]
+                patcharray = [x,y,z,ddm[m],ddl[L],ddz]
                 count=count+1
-
-
-
 
     elif y1 == y2:
         count=1
-        print('YPLANE',NL, Nm, x1, y1, z1)
-        for L in range(1,NL):
+        print('YPLANE',Nl, Nm, x1, y1, z1)
+        for L in range(1,Nl):
             for m in range(1,Nm):
-                x = x1-0.5*ddm(m) + SUM(ddm(1:m))
-                z = z1-.5*ddL(L)+SUM(ddL(1:L))
+                x = x1-0.5*ddm[m] + sum(ddm[1:m])
+                z = z1-.5*ddl[L]+sum(ddl[1:L])
                #sum here
-                y = (-d-normal(1)*x-normal(3)*z)/normal(2)
+                y = (-d-normal[0]*x-normal[2]*z)/normal[1]
                 if(m == 1):
-                    ycenter=y
+                    ycenter = y
                 if(L == 1):
                     ddy = y-y1
-                else
+                else:
                     ddy = 0.5*(y-ycenter)
                 if(z > slope*x+b) or (z < slope1*x+b1):
 #                  GOTO 5
@@ -90,10 +90,28 @@ def CREATEPATCHARRAY(ddm,ddL,Nm,ddL,x1,x2,y1,y2,z1,z2,patcharray,slope,b,slope1,
 #               patcharray(count,4)=ddm(m)
 #               patcharray(count,5)=ddy
 #                patcharray(count,6)=ddl(l)
-                patcharray = [x,y,z,ddm(m),ddy,ddL(L)]
+                patcharray = [x,y,z,ddm[m],ddy,ddL[L]]
                 count=count+1
                 #count is used to move thru patcharray string
                 #I think
+    elif x1 == x2:
+        count = 1
+        print('XPLANE', Nl, Nm, x1, y1, z1)
+        for L in range (1, Nl):
+            for m in range(1, Nm):
+                y = y1- .5*ddm[m] + sum(ddm[1:m])
+                z = z1- .5*ddl[l] + sum(dd1[1:l])
+                x = (-d-normal[2]*z-normal[1]*y)/normal[0]
+
+                if m == 1:
+                    xcenter=x
+                if(L == 1):
+                    ddx= x-x1
+                else:
+                    ddx= 0.5*(x-xcenter)
+                if(z > slope*y+b) or (z < slope1*y+b1):
+                    patcharray = [x,y,z,ddx,ddm[m],ddl[l]]
+                    count= count+1
 
 # 5          CONTINUE
 # 4       CONTINUE
