e1139425c3f8555cb380973781979038ca76ab21
George <seatonk257@gmail.com>
2019-07-22 20:25:56 -0400

    Fixed matlab

---
+++ Matlab_Scripts_Redux/readme.txt
@@ -0,0 +1,4 @@
+I got an old version of our matlab scripts running again. This is less broken.
+I also tried to generalize the code enough that you can run it on computers that aren't mine. 
+Just run DataGraphingTest.m from matlab and you should get the five graphs we had before.
+-G
--- RayTrace.py
+++ RayTrace.py
@@ -438,22 +438,40 @@ print('time: ', time.time()-t)
     # Outputting graphs
 t = time.time()
 
+#######################################################################
 # Will eventually be moved to a receiver function,
 # here now for ease of access of others reading this 
+#######################################################################
+import matplotlib.font_manager as fm
+
+# Font
+stdfont = fm.FontProperties()
+stdfont.set_family('serif')
+stdfont.set_name('Times New Roman')
+stdfont.set_size(20)
+
 for R in ears:
      # For N wave
     pressure = R.signal
     i = R.recNumber
     #plt.figure(i)
-    plt.figure(num = i, figsize=(19.20, 10.80), dpi=120, facecolor='#eeeeee', edgecolor='r')
+    #plt.figure(num = i, figsize=(19.20, 10.80), dpi=120, facecolor='#eeeeee', edgecolor='r')   # grey
+    #plt.figure(num = i, figsize=(19.20, 10.80), dpi=120, facecolor='#e0dae6', edgecolor='r')   # muted lilac
+    plt.figure(num = i, figsize=(19.20, 10.80), dpi=120, facecolor='#e6e6fa', edgecolor='r')    # lavender
     #plt.plot(timeArray,pressure,'r--')
     plt.grid(True)
     plt.plot(timeArray,pressure,'#780303')
         # Labeling axes
-    plt.xlabel('Time [s]')
-    plt.ylabel('Pressure [Pa]')
-    plt.title('Pressure vs Time of Receiver '+ str(i))
+    plt.xlabel('Time [s]', fontproperties=stdfont)
+    plt.ylabel('Pressure [Pa]', fontproperties=stdfont)
+    plt.title('Pressure vs Time of Receiver '+ str(i),
+                fontproperties=stdfont,
+                fontsize=26,
+                fontweight='bold')
+
         # Saving
-    plt.savefig(Pf.graphName + str(i) + '.png')
+    #plt.savefig(Pf.graphName + str(i) + '.png', facecolor='#eeeeee')    # grey
+    #plt.savefig(Pf.graphName + str(i) + '.png', facecolor='#e0dae6')    # muted lilac
+    plt.savefig(Pf.graphName + str(i) + '.png', facecolor='#e6e6fa')    # lavender
     print('Saved receiver', i)
 print('Graph time: ', time.time()-t)
