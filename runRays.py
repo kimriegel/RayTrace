63cdb59dabf06a21628d09a720eb769fe7bb2f33
Robert M. Lefkowitz <r0ml@mac.com>
2019-08-16 00:13:37 -0400

    moving python sources into subdirectory and adding a launch script
    runRays.py   should run RayTrace

---
+++ runRays.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    sys.path.insert(0, "RayTrace")
+    sys.path.insert(0, "venv/lib/python3.7/site-packages")
+    import RayTrace
+
